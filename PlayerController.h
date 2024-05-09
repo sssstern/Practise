615084a59a4ba041855b6540bcb4fc8b73290905
sssstern <koha1739@mail.ru>
2024-05-10 00:33:00 +0300

    in progress

---
+++ PlayerController.h
@@ -0,0 +1,20 @@
+#pragma once
+
+class Player;
+
+class PlayerController {
+private:
+    PlayerController() = default;
+
+    static PlayerController* controller;
+
+public:
+
+    PlayerController(PlayerController const&) = delete;
+    void operator=(PlayerController const&) = delete;
+    ~PlayerController();
+
+    static PlayerController* getPlayerController();
+
+    void controllPlayer(Player* player, float time);
+};
