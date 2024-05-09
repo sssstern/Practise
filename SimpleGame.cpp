f8f7cc93da1c755bae05a01329cfce672ed71ffe
sssstern <koha1739@mail.ru>
2024-05-10 00:19:45 +0300

    working..

--- SimpleGame.cpp
+++ SimpleGame.cpp
@@ -2,31 +2,27 @@
 //
 
 #include "SimpleGame.h"
+#include "Constants.h"
 
 using namespace std;
 
 #include <SFML/Graphics.hpp>
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(400, 400), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setPosition(100, 100);
-    shape.setFillColor(sf::Color(200, 30, 10));
+int main() {
+    sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Title");
 
-    while (window.isOpen())
-    {
+    while (window.isOpen()) {
         sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
                 window.close();
+            }
         }
 
-        window.clear(sf::Color(20, 5, 5));
-        window.draw(shape);
+        window.clear(sf::Color::White);
         window.display();
     }
 
     return 0;
 }
+
